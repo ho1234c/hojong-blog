f683a70fe5e6b15e4c38b5e7675047bc4268fa23
JongHo Jeong <ho1234c@dano.me>
2020-03-24 21:59:50 +0900

    WIP

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,7 @@ module.exports = {
     author: `Jongho Jeong`,
   },
   plugins: [
+    `gatsby-plugin-emotion`,
     `gatsby-plugin-typescript`,
     `gatsby-plugin-react-helmet`,
     {
@@ -66,12 +67,6 @@ module.exports = {
         ],
       },
     },
-    {
-      resolve: `gatsby-plugin-emotion`,
-      options: {
-        // Accepts all options defined by `babel-plugin-emotion` plugin.
-      },
-    }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
